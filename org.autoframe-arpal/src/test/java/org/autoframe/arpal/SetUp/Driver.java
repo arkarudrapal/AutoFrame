1227e00c161311803d3fb2c8524255aa00c6a872
Arkaprabha <arkarudrapal@gmail.com>
2019-07-26 14:48:08 +0530

    Commit - 26/7

--- org.autoframe-arpal/src/test/java/org/autoframe/arpal/SetUp/Driver.java
+++ org.autoframe-arpal/src/test/java/org/autoframe/arpal/SetUp/Driver.java
@@ -2,23 +2,55 @@ package org.autoframe.arpal.SetUp;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import java.util.logging.Logger;
 
 public class Driver {
 
 	private WebDriver driver;
-
+	String browserName;
+	
+	Logger log = Logger.getLogger(Driver.class.getName());
+	
 	public WebDriver getDriver() {
 		return driver;
 	}
 
 	public void setDriver(WebDriver driver) {
 		this.driver = driver;
-	}	
+	}
 
-	public void initDriver() {
-		System.setProperty("webdriver.chrome.driver", 
-				System.getProperty("user.dir")+"\\src\\test\\resources\\driver\\chromedriver.exe");
-		this.driver = new ChromeDriver();
+	public void initDriver(String browserName) {
+		final String ROOTDIR = "user.dir";
+		switch(browserName) {
+			case "chrome":
+				System.setProperty("webdriver.chrome.driver", 
+						System.getProperty(ROOTDIR)+"\\src\\test\\resources\\driver\\chromedriver.exe");
+				this.driver = new ChromeDriver();
+				clearCache();
+				break;
+			case "firefox":
+				System.setProperty("webdriver.gecko.driver", 
+						System.getProperty(ROOTDIR)+"\\src\\test\\resources\\driver\\geckodriver.exe");
+				this.driver = new FirefoxDriver();
+				clearCache();
+				break;
+			case "ie":
+				System.setProperty("webdriver.ie.driver", 
+						System.getProperty(ROOTDIR)+"\\src\\test\\resources\\driver\\IEDriverServer.exe");
+				this.driver = new InternetExplorerDriver();
+				clearCache();
+				break;
+			default:
+				log.info("**************Invalid browser name***************");
+				break;
+		}		
+	}
+
+	public void clearCache() {
+		this.driver.manage().deleteAllCookies();
+		this.driver.manage().window().maximize();
 	}
 
 }
--- org.autoframe-arpal/src/test/java/org/autoframe/arpal/SetUp/Trigger.java
+++ org.autoframe-arpal/src/test/java/org/autoframe/arpal/SetUp/Trigger.java
@@ -3,6 +3,8 @@ package org.autoframe.arpal.SetUp;
 public class Trigger {
 
 	Driver driverobject;
+	
+	String browserValue = System.getProperty("browser");
 
 	public Trigger(Driver driverobject) {
 		this.driverobject = driverobject;
@@ -10,7 +12,8 @@ public class Trigger {
 
 	@cucumber.api.java.Before
 	public void setUp() {
-		driverobject.initDriver();
-		driverobject.getDriver().get("https://www.google.com");
+		driverobject.initDriver(browserValue);
+		driverobject.getDriver().get("https://www.google.com");		
 	}
+	
 }
